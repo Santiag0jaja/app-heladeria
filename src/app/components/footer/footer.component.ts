abee11464e3ee709bacc979457c30e3738009659
Santiag0jaja <santiagodiazeo@gmail.com>
2025-06-13 14:25:09 -0500

    inicio y footer listo (footer no works)

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
+// footer.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,18 +1,18 @@
+// inicio.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  standalone: true,
-  imports: [CommonModule],
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
+  imports: [CommonModule]
 })
 export class InicioComponent {
-  sabores = [
-    { nombre: 'Vainilla', descripcion: 'Clásico sabor a vainilla', precio: 2.5, image : 'https://www.loveandlemons.com/wp-content/uploads/2020/06/vanilla-ice-cream.jpg' },
-    { nombre: 'Chocolate', descripcion: 'Delicioso chocolate belga', precio: 3.0, image : 'https://www.loveandlemons.com/wp-content/uploads/2020/06/chocolate-ice-cream.jpg' },
-    { nombre: 'Fresa', descripcion: 'Fresas naturales', precio: 2.8, image : 'https://www.loveandlemons.com/wp-content/uploads/2020/06/strawberry-ice-cream.jpg' },
-    { nombre: 'Menta', descripcion: 'Refrescante sabor a menta', precio: 3.2, Image: 'https://static.guiainfantil.com/pictures/recetas/5109-helado-de-menta-con-chocolate-postre-refrescante-y-ligero.jpg' },
+  saboresDestacados = [
+    { nombre: 'Chocolate', imagen: 'assets/chocolate.jpg' },
+    { nombre: 'Vainilla', imagen: 'assets/vainilla.jpg' },
+    { nombre: 'Fresa', imagen: 'assets/fresa.jpg' },
+    { nombre: 'Menta', imagen: 'assets/menta.jpg' }
   ];
 }
